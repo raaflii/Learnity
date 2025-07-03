13e69a907de9544a446c97c3b4f6358a74580484
raaflii <rafliwrzky@gmail.com>
2025-07-03 16:01:28 +0700

    change to link instead of vite for css

--- resources/views/landing/landing-page.blade.php
+++ resources/views/landing/landing-page.blade.php
@@ -10,7 +10,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
         rel="stylesheet">
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BBvuUIXV.css') }}">
     <title>Learnity</title>
 </head>
 
